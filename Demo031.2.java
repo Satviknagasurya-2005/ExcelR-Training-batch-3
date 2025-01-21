7d16fd490e26d92375ba10630c0350be590fb58c
Satvik Naga Surya <satviknagasurya@gmail.com>
2025-01-21 12:41:54 +0530

    Hashtable

---
+++ Demo031.2.java
@@ -0,0 +1,17 @@
+
+import java.util.HashSet;
+import java.util.Hashtable;
+
+class HashDemo1 {
+    void main() {
+        HashSet<Object> hSet = new HashSet<>();
+        HashSet.add("Surya");
+        HashSet.add("Krishna");
+        HashSet.add("Revanth");
+        System.out.println(hashset);
+
+        for (Object x: HAshSet) {
+            System.out.println(x);
+        }
+    }
+}
