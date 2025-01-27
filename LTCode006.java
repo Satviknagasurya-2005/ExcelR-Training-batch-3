8205f6664222d4b91d28f512085e34638cfa837e
Satvik Naga Surya <satviknagasurya@gmail.com>
2025-01-27 13:00:08 +0530

    LEAT CODE REMOVING DUPLICATES

---
+++ LTCode006.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+
+        for (int x : nums) {
+            if (k == 0 || x != nums[k - 1]) {
+                nums[k++] = x;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {1, 1, 2, 2, 3, 4, 4, 5};
+        
+        System.out.println("Original array: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        
+        int length = solution.removeDuplicates(nums);
+        
+        System.out.println("\nArray after removing duplicates: ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println("\nLength of array after removing duplicates: " + length);
+    }
+}
