760ebd93ed7c3d4e4e7735f6c5f2dd74b1b66ecb
Satviknagasurya-2005 <n5311907satvik@gmail.com>
2025-02-06 12:43:35 +0530

    print to add 1st and last element of an array

---
+++ Demo063.java
@@ -0,0 +1,8 @@
+public class Demo063 {
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int sum = arr[0] + arr[arr.length - 1];
+        System.out.println("The sum of the first and last element of the array is: " + sum);
+        }
+    }
